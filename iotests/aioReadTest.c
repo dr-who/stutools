0715741a09de327436a6893005eada51b8f81876
Stuart Inglis <stuart.inglis@gmail.com>
2017-06-23 13:59:37 +1200

    added jump option

--- iotests/aioReadTest.c
+++ iotests/aioReadTest.c
@@ -22,26 +22,30 @@ char   *path = NULL;
 int    seqFiles = 0;
 double maxSizeGB = 0;
 int    BLKSIZE=65536;
+size_t jumpStep = 1;
 
 void handle_args(int argc, char *argv[]) {
   int opt;
   
-  while ((opt = getopt(argc, argv, "dDr:t:k:o:q:f:s:G:")) != -1) {
+  while ((opt = getopt(argc, argv, "dDr:t:k:o:q:f:s:G:j:")) != -1) {
     switch (opt) {
     case 't':
       exitAfterSeconds = atoi(optarg);
       break;
     case 'q':
-      qd = atoi(optarg);
+      qd = atoi(optarg); if (qd < 1) qd = 1;
       break;
     case 's':
-      seqFiles = atoi(optarg);
+      seqFiles = atoi(optarg); if (seqFiles < 0) seqFiles = 0;
+      break;
+    case 'j':
+      jumpStep = atoi(optarg); if (jumpStep < 1) jumpStep = 1;
       break;
     case 'G':
-      maxSizeGB = atof(optarg);
+      maxSizeGB = atof(optarg); 
       break;
     case 'k':
-      BLKSIZE = 1024 * atoi(optarg);
+      BLKSIZE = 1024 * atoi(optarg); if (BLKSIZE < 1024) BLKSIZE=1024;
       break;
     case 'f':
       path = optarg;
@@ -51,7 +55,9 @@ void handle_args(int argc, char *argv[]) {
     }
   }
   if (path == NULL) {
-    fprintf(stderr,"./aioReadTest [-s sequentialFiles] [-k blocksizeKB] [-q queueDepth] [-t 30 secs] [-G 32] -f blockdevice\n");
+    fprintf(stderr,"./aioReadTest [-s sequentialFiles] [-j jumpBlocks] [-k blocksizeKB] [-q queueDepth] [-t 30 secs] [-G 32] -f blockdevice\n");
+    fprintf(stderr,"\nExample:\n");
+    fprintf(stderr,"  ./aioReadTest -f /dev/nbd0 -k4 -q64 -s32 -j16 -G100\n");
     exit(1);
   }
 }
@@ -77,19 +83,31 @@ int main(int argc, char *argv[]) {
   const size_t num = 10*1000*1000;
   size_t *positions = malloc(num * sizeof(size_t));
   const size_t gap = bdSize / (seqFiles + 1);
+  size_t *ppp = calloc(seqFiles, sizeof(size_t));
+  for (size_t i = 0; i < seqFiles; i++) {
+    ppp[i] = i * gap;
+    //    fprintf(stderr,"ppp %zd\n", ppp[i]);
+  }
 
   for (size_t i = 0; i < num; i++) {
     if (seqFiles == 0) {
+      // random
       positions[i] = ((size_t)((rand() % bdSize) / BLKSIZE)) * BLKSIZE;
     } else {
-      positions[i] = ((i % seqFiles) * gap) + ((i / seqFiles) * BLKSIZE);
+      // sequential
+      positions[i] = ppp[i % seqFiles];
+      ppp[i % seqFiles] += (jumpStep * BLKSIZE);
+      //((i% seqFiles) * gap) + ((i/ seqFiles) * BLKSIZE);
     }
-    if (i < 10) {
+    if (i < seqFiles + 8) {
       fprintf(stderr,"[%zd/%zd] pos %zd (%.1lf%%)\n", i+1, num, positions[i], positions[i]*100.0/bdSize);
     }
   }
   
   readMultiplePositions(fd, positions, num, BLKSIZE, exitAfterSeconds, qd);
+
+  free(positions);
+  free(ppp);
   
   return 0;
 }
