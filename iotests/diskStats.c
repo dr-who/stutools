5699e68b4910cb507eb0be5063e61f44a8c3da10
Stuart Inglis <stuart.inglis@gmail.com>
2017-08-15 14:56:13 +1200

    More output

--- iotests/diskStats.c
+++ iotests/diskStats.c
@@ -24,7 +24,7 @@ void diskStatAddFinish(diskStatType *d, size_t readSectors, size_t writeSectors)
 void diskStatSummary(diskStatType *d, size_t *totalReadBytes, size_t *totalWriteBytes, size_t shouldReadBytes, size_t shouldWriteBytes, int verbose) {
   *totalReadBytes = (d->finisSecRead - d->startSecRead) * 512;
   *totalWriteBytes =(d->finisSecWrite - d->startSecWrite) * 512;
-  if (verbose && shouldReadBytes && shouldWriteBytes) {
+  if (verbose && (shouldReadBytes || shouldWriteBytes)) {
     fprintf(stderr,"*info* read  amplification: should be %zd, read  %zd, %.2lf%%\n", shouldReadBytes, *totalReadBytes, *totalReadBytes*100.0/shouldReadBytes);
     fprintf(stderr,"*info* write amplification: should be %zd, write %zd, %.2lf%%\n", shouldWriteBytes, *totalWriteBytes, *totalWriteBytes*100.0/shouldWriteBytes);
   }
