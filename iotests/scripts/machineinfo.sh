6571ee05fe8dd6bbf83448725ddafefa5a32b811
Stuart Inglis <stuart.inglis@gmail.com>
2018-01-17 16:24:18 +1300

    Also run test

--- iotests/scripts/machineinfo.sh
+++ iotests/scripts/machineinfo.sh
@@ -5,3 +5,13 @@ free
 cat /proc/cpuinfo | grep name
 dmidecode | grep -i "clock speed"
 dmidecode | grep -i "current speed"
+
+mkdir -p /mnt/stu
+mount -t tmpfs -o size=1024M,mode=0755 tmpfs /mnt/stu
+dd if=/dev/zero of=/mnt/stu/wow >/dev/null 2>&1
+losetup /dev/loop7 /mnt/stu/wow
+../aioRWTest -w -f /dev/loop7 -t2 2>&1 | grep -i total
+../aioRWTest -r -f /dev/loop7 -t2 2>&1 | grep -i total
+losetup -d /dev/loop7
+/bin/rm -f /mnt/stu/wow
+umount /mnt/stu
