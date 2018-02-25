11524299ef34110bc83a38d53aa3dd2ef9538154
Stuart Inglis <stuart.inglis@gmail.com>
2018-02-25 17:09:08 +1300

    Tidied up and start each thread at the same time

--- iotests/aioMulti.c
+++ iotests/aioMulti.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <getopt.h>
 
 #include <pthread.h>
@@ -21,23 +22,57 @@ int verbose = 0;
 int flushEvery = 0;
 
 typedef struct {
+  size_t fd;
   size_t id;
-  positionType *positions;
-  size_t numpositions;
+  size_t qd;
+  size_t max;
+  float readRatio;
+  size_t total;
 } threadInfoType;
 
 static size_t blockSize = 65536*4;
 
+volatile int ready = 0;
+double startTime = 0;
+
+void intHandler(int d) {
+  fprintf(stderr,"got signal\n");
+  keepRunning = 0;
+
+}
+
 static void *runThread(void *arg) {
   threadInfoType *threadContext = (threadInfoType*)arg;
   //  fprintf(stderr,"id: %zd\n", threadContext->id);
-
+  
+  size_t bdsize = blockDeviceSizeFromFD(threadContext->fd);
+  //  fprintf(stderr,"[thread %zd] bdSize %zd (%.2lf GiB)\n", threadContext->id, bdsize, TOGiB(bdsize));
+  
+  size_t positionsNum = 100000;
+  positionType *positions = createPositions(positionsNum);
+  
+  int fdA[1];
+  fdA[0] = threadContext->fd;
+  //    fprintf(stderr,"fd %d\n", fdA[0]);
+  setupPositions(positions, &positionsNum, fdA, 1, bdsize, 1, threadContext->readRatio, blockSize, blockSize, blockSize, 0, 0, 1, bdsize, 0, NULL);
+  
+  
   char *randomBuffer = aligned_alloc(blockSize, blockSize); if (!randomBuffer) {fprintf(stderr,"oom!\n");exit(-1);}
   generateRandomBuffer(randomBuffer, blockSize);
-
+  
   size_t ios, trb, twb;
-  size_t qd = 256;
-  aioMultiplePositions(threadContext->positions, threadContext->numpositions, 100, qd, 0, 0, NULL, NULL, randomBuffer, blockSize, blockSize, &ios, &trb, &twb, 0);
+  ready++;
+  while (ready != threadContext->max) {
+    usleep(100);
+  }
+  if (threadContext->id == 0) {
+    startTime = timedouble();
+  }
+  //  fprintf(stderr,"go(%zd)\n",threadContext->id);
+  threadContext->total = aioMultiplePositions(positions, positionsNum, 100, threadContext->qd, 0, 0, NULL, NULL, randomBuffer, blockSize, blockSize, &ios, &trb, &twb, 0);
+
+  free(positions);
+
 
   return NULL;
 }
@@ -45,7 +80,7 @@ static void *runThread(void *arg) {
 
 
 
-void startThreads(positionType **positions, size_t *numpositions, size_t num) {
+void startThreads(size_t *fdArray, size_t num, size_t qd, float rr) {
   
   pthread_t *pt;
   CALLOC(pt, num, sizeof(pthread_t));
@@ -56,36 +91,45 @@ void startThreads(positionType **positions, size_t *numpositions, size_t num) {
   assert(threadContext);
 
   for (size_t i = 0; i < num; i++) {
+    threadContext[i].fd = fdArray[i];
     threadContext[i].id = i;
-    threadContext[i].positions = positions[i];
-    threadContext[i].numpositions = numpositions[i];
+    threadContext[i].readRatio = rr;
+    threadContext[i].qd = qd;
+    threadContext[i].max = num;
     pthread_create(&(pt[i]), NULL, runThread, &(threadContext[i]));
   }
 
   for (size_t i = 0; i < num; i++) {
     pthread_join(pt[i], NULL);
   }
+
+  double elapsed = timedouble() - startTime;
+  size_t sum = 0;
+  for (size_t i = 0; i <num;i++) {
+    sum += threadContext[i].total;
+  }
+  fprintf(stderr,"*info* summary: readRatio %.1lf, total bytes %zd, %.1lf secs, %.2lf GiB/s\n", rr, sum, elapsed, TOGiB(sum) / elapsed);
   free(threadContext);
   free(pt);
 }
 
 
-double readRatio = 0.5;
+double readRatio = 1;
 
 int handle_args(int argc, char *argv[]) {
 
   if (argc <= 1) {
-    fprintf(stderr,"./aioMulti [-r] [-w] ...devices...\n");
+    fprintf(stderr,"./aioMulti [-r (default)] [-w] ...devices...\n");
     exit(-1);
   } else {
     int opt;
     while ((opt = getopt(argc, argv, "rw")) != -1) {
       switch (opt) {
       case 'r':
-	readRatio += 0.5;
+	readRatio = 1;
 	break;
       case 'w':
-	readRatio -= 0.5;
+	readRatio = 0;
 	break;
       default:
 	break;
@@ -97,6 +141,8 @@ int handle_args(int argc, char *argv[]) {
 
 
 int main(int argc, char *argv[]) {
+  signal(SIGTERM, intHandler);
+  signal(SIGINT, intHandler);
 
   int opt = handle_args(argc, argv);
 
@@ -106,34 +152,19 @@ int main(int argc, char *argv[]) {
   for (size_t i = opt; i < argc; i++) {
     // add device
     int fd = open(argv[i], O_RDWR | O_DIRECT | O_TRUNC | O_EXCL);
-	fprintf(stderr,"%s\n", argv[i]);
-    assert(fd>=0);
+    //	fprintf(stderr,"%s\n", argv[i]);
+    if (fd <= 0) {
+      perror(argv[i]);
+      exit(-1);
+    }
     fdArray[fdCount++] = fd;
   }
 
-  positionType **positions;
-  size_t *positionsNum;
-  CALLOC(positions, fdCount, sizeof(positionType*));
-  CALLOC(positionsNum, fdCount, sizeof(size_t));
+  const size_t qd = 30000 / fdCount;
+  fprintf(stderr,"*info* readRatio %.1f, %d devices, each with qd=%zd (30,000/%d)\n", readRatio, fdCount, qd, fdCount);
+  
+  startThreads(fdArray, fdCount, qd, readRatio);
 
-
-  for (size_t i = 0; i < fdCount; i++) {
-    size_t bdsize = blockDeviceSizeFromFD(fdArray[i]);
-    //    fprintf(stderr,"%zd: %zd\n", i, bdsize);
-
-    positionsNum[i] = 1000000 + i;
-    positions[i] = createPositions(positionsNum[i]);
-
-    int fdA[1];
-    fdA[0] = fdArray[i];
-    //    fprintf(stderr,"fd %d\n", fdA[0]);
-    setupPositions(positions[i], &(positionsNum[i]), fdA, 1, bdsize, 1, readRatio, blockSize, blockSize, blockSize, 0, 0, 1, bdsize, 0, NULL);
-  }
-
-  startThreads(positions, positionsNum, fdCount);
-
-  free(positions);
-  free(positionsNum);
   free(fdArray);
 
   // create numDevices threads, call aioOperations per thread/device
