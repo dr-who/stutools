6e6ffc0b97f8c2d02e1d466ab011be33a8a14cf3
Stuart Inglis <stuart.inglis@gmail.com>
2018-03-23 13:05:02 +1300

    Check proc vm setup first

--- iotests/ramEater.c
+++ iotests/ramEater.c
@@ -16,25 +16,26 @@ int main(int argc, char *argv[]) {
     exit(-1);
   }
 
-  FILE *fp = fopen("/proc/sys/vm/overcommit_memory", "rt");
-  if (!fp) {perror("overcommit_memory");exit(-1);}
+  FILE *fp = fopen("/proc/sys/vm/overcommit_ratio", "rt");
+  if (!fp) {perror("overcommit_ratio");exit(-1);}
   size_t value = 0;
   int f = fscanf(fp,"%zd", &value);
+  if (f==1) {
+    fprintf(stderr,"*info* overcommit_ratio is %zd\n", value);
+  }
+  fclose(fp);
+  if (value != 100) {fprintf(stderr,"*error* the value needs to be 100\n  echo 100 > /proc/sys/vm/overcommit_ratio\n");exit(-1);}
+
+  fp = fopen("/proc/sys/vm/overcommit_memory", "rt");
+  if (!fp) {perror("overcommit_memory");exit(-1);}
+  value = 0;
+  f = fscanf(fp,"%zd", &value);
   if (f==1) {
     fprintf(stderr,"*info* overcommit_memory is %zd\n", value);
   }
   fclose(fp);
   if (value != 2) {fprintf(stderr,"*error* the value needs to be 2. \n  echo 2 > /proc/sys/vm/overcommit_memory\n");exit(-1);}
   
-  fp = fopen("/proc/sys/vm/overcommit_ratio", "rt");
-  if (!fp) {perror("overcommit_ratio");exit(-1);}
-  value = 0;
-  f = fscanf(fp,"%zd", &value);
-  if (f==1) {
-    fprintf(stderr,"*info* overcommit_ratio is %zd\n", value);
-  }
-  fclose(fp);
-  if (value != 100) {fprintf(stderr,"*error* the value needs to be 100\n  echo 100 > /proc/sys/vm/overcommit_ratio\n");exit(-1);}
 
   size_t maxArrays = 1024*1024;
   void **p;
