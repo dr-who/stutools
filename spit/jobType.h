540b2ffaea76a1c4a63493147a1e768cf3ac56ec
Stuart Inglis <stuart.inglis@gmail.com>
2019-06-04 18:27:35 +1200

    Added seed

--- spit/jobType.h
+++ spit/jobType.h
@@ -16,7 +16,7 @@ void jobAdd(jobType *j, const char *jobstring);
 void jobDump(jobType *j);
 void jobFree(jobType *j);
 size_t jobCount(jobType *j);
-void jobRunThreads(jobType *job, const int num, const size_t maxSizeInBytes, const size_t timetorun, const size_t dumpPos, char *benchmarkName, const size_t origqd);
+void jobRunThreads(jobType *job, const int num, const size_t maxSizeInBytes, const size_t timetorun, const size_t dumpPos, char *benchmarkName, const size_t origqd, unsigned short seed);
 void jobMultiply(jobType *job, const size_t extrajobs, deviceDetails *deviceList, size_t deviceCount);
 void jobAddDeviceToAll(jobType *j, const char *device);
 
