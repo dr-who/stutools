b0584b82b2ee14d9ed55da2d89e91ee38685d61e
Stuart Inglis <stuart.inglis@gmail.com>
2019-06-25 21:43:42 +1200

    use p% for random

--- spit/fuzz.c
+++ spit/fuzz.c
@@ -14,20 +14,20 @@ char *randomCommandString(const double rwratio) {
   int s = lrand48() % 5;
   switch (s) {
   case 0: 
-    sprintf(string, "%cs%ld", (drand48() < rwratio) ? 'r' : 'w', lrand48() % 100);
+    sprintf(string, "p%.1lfs%ld", rwratio, lrand48() % 100);
     break;
   case 1: 
-    sprintf(string, "%cP%ld%c", (drand48() < rwratio) ? 'r' : 'w', 1+lrand48() % 10000, (drand48() < 0.5) ? 'n' : 'N');
+    sprintf(string, "p%.1lfcP%ld%c", rwratio, 1+lrand48() % 10000, (drand48() < 0.5) ? 'n' : 'N');
     break;
   case 2: 
-    sprintf(string, "%cP%ldx%ld", (drand48() < rwratio) ? 'r' : 'w', 1+lrand48() % 10000, 1 + lrand48()%100);
+    sprintf(string, "p%.1lfP%ldx%ld", rwratio, 1+lrand48() % 10000, 1 + lrand48()%100);
     break;
   case 3: 
-    sprintf(string, "%ck%ld", (drand48()< rwratio) ? 'r' : 'w', 4 * (1+ (lrand48() % 4)));
+    sprintf(string, "p%.1lfk%ld", rwratio, 4 * (1+ (lrand48() % 4)));
     break;
   case 4: {}
     size_t klow = 4 * (1 + (lrand48() % 3));
-    sprintf(string, "%ck%ld-%ld", (drand48() < rwratio) ? 'r' : 'w', klow, klow + 4*(lrand48()%4));
+    sprintf(string, "p%.1lfk%ld-%ld", rwratio, klow, klow + 4*(lrand48()%4));
     break;
   }
   return strdup(string);
@@ -61,15 +61,15 @@ char ** fuzzString(int *argc, const char *device) {
   argv[3] = strdup(string);
 
   argv[4] = strdup("-c");
-  argv[5] = randomCommandString(0.5);
+  argv[5] = randomCommandString(0.1);
 
-  if (drand48() < 0.5) {
+  if (drand48() < 0.2) {
     argv[6] = strdup("-j");
     sprintf(string, "%zd", 1+lrand48() % 8);
     argv[7] = strdup(string);
   } else {
     argv[6] = strdup("-c");
-    argv[7] = randomCommandString(0.5);
+    argv[7] = randomCommandString(0.9);
   }
 
   fprintf(stderr,"*info* random command: ");
