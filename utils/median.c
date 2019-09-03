6356151128d31f8dc402b92d622224c6923363de
Stuart Inglis <stuart.inglis@gmail.com>
2019-09-03 14:02:03 +1200

    Fixed

--- utils/median.c
+++ utils/median.c
@@ -5,25 +5,28 @@
 #include <limits.h>
 #include <assert.h>
 
+
+typedef struct {
+  double value;
+  size_t age;
+} pointType;
+
 typedef struct {
   size_t num;
   size_t sorted;
-  double *values;
-  size_t *age;
+  pointType *values;
   size_t ever;
 } numListType;
 
 void nlInit(numListType *n) {
   n->num = 0;
   n->values = NULL;
-  n->age = NULL;
   n->sorted = 0;
   n->ever = 0;
 }
 
 void nlFree(numListType *n) {
   if (n->values) free(n->values);
-  if (n->age) free(n->age);
   nlInit(n);
 }
 
@@ -34,22 +37,22 @@ size_t nlAdd(numListType *n, double value) {
   size_t addat = n->num;
 
   if (n->num < 100) {
-    n->values = realloc(n->values, (n->num+1) * sizeof(double));
-    n->age = realloc(n->age, (n->num+1) * sizeof(size_t));
+    n->values = realloc(n->values, (n->num+1) * sizeof(pointType));
     n->num++;
   } else {
-    size_t lowest = (size_t)-1;
+    size_t lowest = n->ever + 1;
     for (size_t i = 0; i < n->num; i++) {
-      if (n->age[i] < lowest) {
-	lowest = n->age[i];
+      fprintf(stdout,"*checking* %zd, value %g age %zd\n", i, n->values[i].value, n->values[i].age);
+      if (n->values[i].age < lowest) {
+	lowest = n->values[i].age;
 	addat = i;
       }
     }
-    //    fprintf(stderr,"replacing oldest at position %zd\n", addat);
+    fprintf(stdout,"replacing oldest at position %zd\n", addat);
   }
 
-  n->values[addat] = value;
-  n->age[addat] = n->ever++;
+  n->values[addat].value = value;
+  n->values[addat].age = n->ever++;
   n->sorted = 0;
 
     //
@@ -59,18 +62,18 @@ size_t nlAdd(numListType *n, double value) {
 
 static int nl_sortfunction(const void *origp1, const void *origp2)
 {
-  double *p1 = (double*)origp1;
-  double *p2 = (double*)origp2;
+  pointType *p1 = (pointType*)origp1;
+  pointType *p2 = (pointType*)origp2;
   
-  if (*p1 < *p2) return -1;
-  else if (*p1 > *p2) return 1;
+  if (p1->value < p2->value) return -1;
+  else if (p1->value > p2->value) return 1;
   else return 0;
 }
 
 
 void nlSort(numListType *n) {
   if (!n->sorted) {
-    qsort(n->values, n->num, sizeof(double), nl_sortfunction);
+    qsort(n->values, n->num, sizeof(pointType), nl_sortfunction);
     n->sorted = 1;
   }
 }
@@ -83,10 +86,10 @@ double nlSortedPos(numListType *n, double pos) {
 
   //  fprintf(stdout,"*info* index %zd (%zd and %lf)\n", i, n->num, pos);
   if ((n->num % 2) == 0) {
-    return (n->values[i] + n->values[i+1]) / 2.0;
+    return (n->values[i].value + n->values[i+1].value) / 2.0;
   } else {
     // odd
-    return n->values[i];
+    return n->values[i].value;
   }
 }
     
@@ -102,7 +105,7 @@ double nlMedian(numListType *n) {
 
 void nlDump(numListType *n) {
   for (size_t i = 0; i < n->num; i++) {
-    fprintf(stdout,"%g\n", n->values[i]);
+    fprintf(stdout,"%g (age %zd)\n", n->values[i].value, n->values[i].age);
   }
 }
 
