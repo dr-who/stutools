74b0b1558eab3953d7fabc54033ffae3acc7ec60
Stuart Inglis <stuart.inglis@gmail.com>
2019-09-03 13:48:44 +1200

    Updated median

--- utils/median.c
+++ utils/median.c
@@ -3,31 +3,55 @@
 
 #include <malloc.h>
 #include <limits.h>
+#include <assert.h>
 
 typedef struct {
   size_t num;
   size_t sorted;
   double *values;
+  size_t *age;
+  size_t ever;
 } numListType;
 
 void nlInit(numListType *n) {
   n->num = 0;
   n->values = NULL;
+  n->age = NULL;
   n->sorted = 0;
+  n->ever = 0;
 }
 
 void nlFree(numListType *n) {
   if (n->values) free(n->values);
+  if (n->age) free(n->age);
   nlInit(n);
 }
 
    
 
 size_t nlAdd(numListType *n, double value) {
-  n->values = realloc(n->values, (n->num+1) * sizeof(double));
-  n->values[n->num] = value;
+
+  size_t addat = n->num;
+
+  if (n->num < 100) {
+    n->values = realloc(n->values, (n->num+1) * sizeof(double));
+    n->age = realloc(n->age, (n->num+1) * sizeof(size_t));
+    n->num++;
+  } else {
+    size_t lowest = (size_t)-1;
+    for (size_t i = 0; i < n->num; i++) {
+      if (n->values[i] < lowest) {
+	lowest = n->values[i];
+	addat = i;
+      }
+    }
+  }
+
+  n->values[addat] = value;
+  n->age[addat] = n->ever++;
   n->sorted = 0;
-  n->num++;
+
+    //
   return n->num;
 }
 
@@ -50,17 +74,25 @@ void nlSort(numListType *n) {
   }
 }
 
-double nlMedian(numListType *n) {
+double nlSortedPos(numListType *n, double pos) {
   nlSort(n);
+  assert(pos>=0);
+  assert(pos < n->num);
+  size_t i = (size_t) ((n->num-1) * pos);
+
+  //  fprintf(stdout,"*info* index %zd (%zd and %lf)\n", i, n->num, pos);
+  if ((n->num % 2) == 0) {
+    return (n->values[i] + n->values[i+1]) / 2.0;
+  } else {
+    // odd
+    return n->values[i];
+  }
+}
+    
+
+double nlMedian(numListType *n) {
   if (n->num > 0) {
-    if ((n->num % 2) == 0) {
-      // even
-      const size_t i = n->num / 2;
-      return (n->values[i] + n->values[i-1]) / 2.0;
-    } else {
-      // odd
-      return n->values[n->num / 2];
-    }
+    return nlSortedPos(n, 0.5);
   } else {
     return 0.0/0.0;
   }
@@ -81,11 +113,11 @@ int main() {
 
   double v = 0;
   while (scanf("%lf", &v) == 1) {
+    //    fprintf(stdout,"*info* in %g\n", v);
     nlAdd(&n, v);
-  }
-
-  if (n.num > 0) {
-    fprintf(stdout,"%g\n", nlMedian(&n));
+    if (n.num > 0) {
+      fprintf(stdout,"%g %g %g %g %g\n", nlSortedPos(&n, 0.001), nlSortedPos(&n, 0.01), nlMedian(&n), nlSortedPos(&n, 0.99), nlSortedPos(&n, 0.999));
+    }
   }
   
   nlFree(&n);
