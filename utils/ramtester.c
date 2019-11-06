7cb93c5e9771f8233fe1d2c10424286f01fcb607
Stuart Inglis <stuart.inglis@gmail.com>
2019-11-06 18:40:30 +1300

    first version

---
+++ utils/ramtester.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+
+#include <malloc.h>
+#include <sys/sysinfo.h>
+#include <sys/time.h>
+#include <string.h>
+
+size_t freeRAM() {
+  struct sysinfo info;
+  sysinfo(&info);
+  return info.freeram + info.bufferram + info.sharedram;
+}
+
+inline double timedouble() {
+  struct timeval now;
+  gettimeofday(&now, NULL);
+  double tm = ((double)now.tv_sec * 1000000.0) + now.tv_usec;
+  return tm/1000000.0;
+}
+
+
+int main(int argc, char *argv[]) {
+  size_t ram = freeRAM();
+
+
+  fprintf(stderr,"*info* ram to test %zd MiB\n", ram/1024/1024);
+
+  unsigned char *c = NULL;
+  do {
+    c = malloc(ram);
+    if (!c) {
+      fprintf(stderr,"No ram in %zd chunk\n", ram);
+      ram = ram * 0.99;
+      //      exit(1);
+    }
+  } while (c == NULL);
+  fprintf(stderr,"*info* allocated %zd chunk\n", ram);
+  
+  unsigned char pattern[3] = {0xff, 0x00, 0x01 + 0x04 + 0x10 + 0x40};
+  for (size_t pat = 0; pat < 3; pat++ ){
+    fprintf(stderr,"*info* setting pattern to %2x\n", pattern[pat]);
+    memset(c, pattern[pat], ram);
+
+    double starttime = timedouble();
+    double runtime = 5* 60;
+    size_t pass = 1;
+    while (timedouble() - starttime < runtime) { // 5 mins
+      fprintf(stderr,"*info* pass %zd looking for non %2x (%.0lf secs remain)\n", pass++, pattern[pat], runtime - (timedouble() - starttime));
+      unsigned char *p = c;
+      for (size_t i = 0; i < ram; i++) {
+	if (*p != pattern[pat]) {
+	  fprintf(stderr,"error at [%zd]: was %2x instead of %2x\n", i, *p, pattern[pat]);
+	}
+	p++;
+      }
+    }
+  }
+  free(c);
+  
+
+  return 0;
+}
