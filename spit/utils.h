a1de306b4db92a6511a81de25bc1db116d2c758a
Stuart Inglis <stuart.inglis@gmail.com>
2020-07-31 10:05:30 +1200

    Added syslog for slow IO

--- spit/utils.h
+++ spit/utils.h
@@ -118,6 +118,7 @@ int pinThread( pthread_t* thread, int* hw_tids, size_t n_hw_tid );
 
 int getDiscardInfo(const char *suffix, size_t *alignment_offset, size_t *discard_max_bytes, size_t *discard_granularity, size_t *discard_zeroes_data);
 int performDiscard(int fd, const char *path, unsigned long low, unsigned long high, size_t max_bytes, size_t discard_granularity, double *maxdelay_secs, const int verbose);
+void syslogString(const char *prog, const char *message);
 
 #endif
 
