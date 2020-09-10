6c2d1218c20da16798a8707b39db8b5a052a887c
Stuart Inglis <stuart.inglis@gmail.com>
2020-09-11 11:52:20 +1200

    Remove needing a sz

--- spit/pmemwreck.c
+++ spit/pmemwreck.c
@@ -15,14 +15,15 @@ int verbose = 0;
 
 int main(int argc, char *argv[])
 {
-  size_t sz = 1024*1024*32;
-  if (argc < 3) {
-    fprintf(stderr,"Usage: ./pmemwreck /dev/dax0.0 sizeInBytes (say $[32*1024*1024])\n");
+  size_t sz = 0;
+  if (argc < 2) {
+    fprintf(stderr,"Usage: ./pmemwreck /dev/dax0.0 [sizeInBytes (say $[32*1024*1024])]\n");
     exit(1);
   }
   const char* dev = argv[1];
-  sz = atol(argv[2]);
-  fprintf(stderr,"*info* opening '%s' (first %zd bytes), wrecking a byte every 4096\n", dev, sz);
+  if (argc == 3) {
+    sz = atol(argv[2]);
+  }
 
   char sys[1000], *suffix = getSuffix(dev);
   sprintf(sys, "/sys/bus/dax/devices/%s/size", suffix);
@@ -30,10 +31,16 @@ int main(int argc, char *argv[])
   size_t maxsz = 0;
   int ret = fscanf(fp, "%lu", &maxsz);
   if (ret != 1) maxsz = 0;
-  fprintf(stderr,"*info* max size of device %zd\n", maxsz);
   if (sz > maxsz) {
     sz = maxsz;
   }
+  if (sz == 0) {
+    sz = maxsz;
+  }
+
+  fprintf(stderr,"*info* logical size of device %zd (%.1lf GiB)\n", maxsz, TOGiB(maxsz));
+  fprintf(stderr,"*info* opening '%s', wrecking %zd bytes (%.1lf GiB), in 4096 steps\n", dev, sz, TOGiB(sz));
+
 
   int fd = open( dev, O_RDWR );
   if( fd < 0 ) {
