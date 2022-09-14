b29944c256d3fe057923141c8a77193b6e14eb5b
Stuart Inglis <stuart.inglis@gmail.com>
2022-09-15 10:50:50 +1200

    better English

--- spit/devices.c
+++ spit/devices.c
@@ -36,7 +36,7 @@ deviceDetails *addDeviceDetails(const char *fn, deviceDetails **devs, size_t *nu
       //      return &(*devs)[i];
     }
     if ((strlen(fnserial) >= 5) && (strcmp(fnserial, fnserial2)==0)) {
-      fprintf(stderr,"*warning* ignoring %s, it shares the same serial number '%s' with %s\n", fn, fnserial2, dcmp->devicename);
+      fprintf(stderr,"*warning* ignoring %s, it shares a serial number '%s' with %s\n", fn, fnserial2, dcmp->devicename);
       return NULL;
       //      return &(*devs)[i];
     }
