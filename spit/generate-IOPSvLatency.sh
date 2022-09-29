fa0e51677ae52c67f8e64a84af82ed9786eec133
Stuart Inglis <stuart.inglis@gmail.com>
2022-09-29 21:06:35 +1300

    new versions

--- spit/generate-IOPSvLatency.sh
+++ spit/generate-IOPSvLatency.sh
@@ -12,17 +12,18 @@ then
     exit 1
 fi
 
-BLOCKSIZEK=1024
+BLOCKSIZEK=4
+MODEL=$(smartctl -a /dev/$TESTDEVICE | grep Model: | sed 's/Device Model: *//g')
 
 echo none >/sys/block/${TESTDEVICE}/queue/scheduler
 echo == nr_requests `cat /sys/block/${TESTDEVICE}/queue/nr_requests` ==
 
 spit -c rs0q2048k${BLOCKSIZEK} -f /dev/${TESTDEVICE} -B maxspeed -t 20
 
-MAXIOPS=$(tail -n +3 maxspeed | gawk '{x+=$4} END {print int(1.5*x/NR)}')
+export MAXIOPS=$(tail -n +3 maxspeed | gawk '{x+=$4} END {print int(1.5*x/NR)}')
 
-STEPS=$(echo $MAXIOPS | awk '{print int($1*1.0/20)}')
-echo === processing block device $TESTDEVICE in steps of $STEPS to $MAXIOPS
+export STEPS=$(echo $MAXIOPS | awk '{print int($1*1.0/20)}')
+echo === $MODEL - processing block device $TESTDEVICE in steps of $STEPS to $MAXIOPS
 
 
 /bin/rm -f IOPSvsLatency.txt
@@ -30,14 +31,14 @@ echo === processing block device $TESTDEVICE in steps of $STEPS to $MAXIOPS
 
 
 
-START=$STEPS
+export START=$STEPS
 
 echo === $START $STEPS ====
 
 while [ $START -le $MAXIOPS ]
 do
     echo === $START ====
-    spit -f /dev/${TESTDEVICE} -c rs0q2048k${BLOCKSIZEK}S${START} -B out.${START} -P pos.${START}
+    spit -f /dev/${TESTDEVICE} -c rs0q2048k${BLOCKSIZEK}S${START} -B out.${START} -P pos.${START} -t 4
 
     START=$(echo $START $STEPS | awk '{print $1+$2}')
 
@@ -48,7 +49,7 @@ START=$STEPS
 while [ $START -le $MAXIOPS ]
 do
     echo == generate $START ====
-    cat pos.${START} | tail -n +2 | awk '{print $14}' | ../utils/dist -a 10000 | awk -v s=${START} '{print s,1000.0*$5,1000.0*$4,1000.0*$6}' | tail -n 1 >> IOPSvsLatency.txt
+    cat pos.${START} | tail -n +2 | awk '{print 1000.0*$14}' | ./stat | awk -v s=${START} '{print s,$6,$4,$8}' | tail -n 1 >> IOPSvsLatency.txt
     cat out.${START} | awk -v s=${START} '{x+=$4} END {print s,1.0*x/NR}' >> IOPSvsLatency-qd.txt
 
     START=$(echo $START $STEPS | awk '{print $1+$2}')
@@ -59,7 +60,7 @@ done
 echo "set xlabel 'Target I/O Operations per Second (IOPS)'" > IOPSvsLatency.gnu
 echo "set ylabel 'Average latency (ms)'" >> IOPSvsLatency.gnu
 echo "set y2label 'Achieved I/O Operations per Second (IOPS)'" >> IOPSvsLatency.gnu
-echo "set title 'Random read ${BLOCKSIZEK}KiB operations'" >> IOPSvsLatency.gnu
+echo "set title 'Random read ${BLOCKSIZEK}KiB operations - $MODEL'" >> IOPSvsLatency.gnu
 echo "set ytics out" >> IOPSvsLatency.gnu
 echo "set xtics out" >> IOPSvsLatency.gnu
 echo "set y2tics out" >> IOPSvsLatency.gnu
