4277d061bb5924501fa1b0eabb4fd7106e418b01
Stuart Inglis <stuart.inglis@gmail.com>
2022-11-29 21:23:16 +1300

    windows source + binary;

--- spit/netsend.c
+++ spit/netsend.c
@@ -3,17 +3,27 @@
 #include <string.h>
 #include <libgen.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 #include <assert.h>
+#include <time.h>
+#include "transfer.h"
+
+#ifdef __linux__
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include "transfer.h"
-#include "utils.h"
+#endif
 
-int keepRunning = 1;
+#ifdef __WIN32
+#include <winsock.h>
+int inet_aton(const char *cp, struct in_addr *addr)
+{
+  addr->s_addr = inet_addr(cp);
+  return (addr->s_addr == INADDR_NONE) ? 0 : 1;
+}
+#endif
 
-void sendfile(FILE *fp, int sockfd);
-ssize_t total=0;
+
+
 int main(int argc, char* argv[])
 {
   if (argc < 2)
@@ -23,6 +33,11 @@ int main(int argc, char* argv[])
     }
     int port = 8877;
 
+#ifdef __WIN32
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+#endif
+    
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
     {
@@ -35,17 +50,21 @@ int main(int argc, char* argv[])
     FILE *fp = fopen("/dev/random", "rb");
     long c = 0;
     if (fread(&c, sizeof(long), 1, fp) == 0) {
-      perror("random");
+      //      perror("random");
+      c = time(NULL);
+      fprintf(stderr,"*info* random seed from clock\n");
     }
     fclose(fp);
     //    fprintf(stderr,"*random* %ld\n", c);
-    srand(timedouble()*100000 + c);
+    srand(c);
     int tries = 0;
     do {
       port = 8877 + rand()%100;
       serveraddr.sin_family = AF_INET;
       serveraddr.sin_port = htons(port);
-      if (inet_pton(AF_INET, argv[1], &serveraddr.sin_addr) < 0)
+
+      if (inet_aton(argv[1], &serveraddr.sin_addr) < 0) 
+	//      if (inet_pton(AF_INET, argv[1], &serveraddr.sin_addr) < 0)
 	{
 	  perror("IPaddress Convert Error");
 	  exit(1);
@@ -73,12 +92,12 @@ int main(int argc, char* argv[])
 
     ssize_t n;
     clock_t lastclock = clock();
-    double lasttime = timedouble();
+    double lasttime = time(NULL);
     while ((n = send(sockfd, buff, BUFFSIZE, 0)) > 0) {
       if (argc > 2) {
-	if (timedouble() - lasttime> 1) {
-	  fprintf(stdout,"*info* [port %d] CPU %.1lf %% (100%% is one core)\n", port, (clock() - lastclock) * 100.0 / (timedouble() - lasttime) / CLOCKS_PER_SEC);
-	  lasttime = timedouble();
+	if (time(NULL) - lasttime> 1) {
+	  fprintf(stdout,"*info* [port %d] CPU %.1lf %% (100%% is one core)\n", port, (clock() - lastclock) * 100.0 / (time(NULL) - lasttime) / CLOCKS_PER_SEC);
+	  lasttime = time(NULL);
 	  lastclock = clock();
 	}
       }
