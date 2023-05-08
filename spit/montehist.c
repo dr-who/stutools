b03cc5bea4ca772c262b15481919278ba578d50e
Stuart Inglis <stuart.inglis@gmail.com>
2023-05-09 10:10:14 +1200

    added help

--- spit/montehist.c
+++ spit/montehist.c
@@ -6,22 +6,43 @@
 #include "histogram.h"
 #include "numList.h"
 
+void usage() {
+  fprintf(stdout,"Usage:\n");
+  fprintf(stdout,"  montehist -i hist [-s n] [-m -M -a] [-r n]\n");
+  fprintf(stdout,"\n");
+  fprintf(stdout,"Options:\n");
+  fprintf(stdout,"  -s sn    # specify the number of samples\n");
+  fprintf(stdout,"  -m       # output the minimum of the <sn> samples\n");
+  fprintf(stdout,"  -a       # output the mean of the <sn> samples\n");
+  fprintf(stdout,"  -M       # output the max of the <sn> samples\n");
+  fprintf(stdout,"  -r n     # perform <n> runs of <sn> samples, as a table\n");
+}
+
 int main(int argc, char *argv[])
 {
   char *histfile = "hist.out";
   size_t samples = 10;
 
   size_t calcMin = 0, calcMax = 0, calcMean = 0;
+
+  size_t rows = 0;
   
   int opt;
-  const char *getoptstring = "i:s:mMa";
+  const char *getoptstring = "i:s:mMar:h";
   while ((opt = getopt(argc, argv, getoptstring)) != -1) {
     switch (opt) {
     case 'a':
       calcMean = 1;
       break;
+    case 'h':
+      usage();
+      exit(1);
+      break;
     case 'i':
       histfile = strdup(optarg);
+      break;
+    case 'r':
+      rows = atoi(optarg); 
       break;
     case 's':
       samples = atoi(optarg);
@@ -42,34 +63,51 @@ int main(int argc, char *argv[])
   histLoad(&h, histfile);
 
   const double consistency = histConsistency(&h);
+  if (samples) fprintf(stderr,"*info* samples = %zd\n", samples);
+  if (rows) fprintf(stderr,"*info* rows = %zd\n", rows);
   fprintf(stderr,"*info* consistency score %.1lf%% using binSize of %zd\n", consistency, h.binScale);
 
   srand48(getDevRandom());
 
-  double theMin = NAN, theMax = NAN, theSum = NAN;
-  for (size_t i = 0; i < samples; i++) {
-    const double sample = histSample(&h);
-    fprintf(stdout, "%lf\n", sample);
-    if (i==0) {
-      theMin = sample;
-      theMax = sample;
-      theSum = sample;
-    } else {
-      if (sample < theMin) theMin = sample;
-      if (sample > theMax) theMax = sample;
-      theSum += sample;
+  size_t r = 0;
+  
+  do {
+    r++;
+    
+    double theMin = NAN, theMax = NAN, theSum = NAN;
+    for (size_t i = 0; i < samples; i++) {
+      const double sample = histSample(&h);
+      if (rows == 0) {
+	fprintf(stdout, "%lf\n", sample);
+      }
+      
+      if (i==0) {
+	theMin = sample;
+	theMax = sample;
+	theSum = sample;
+      } else {
+	if (sample < theMin) theMin = sample;
+	if (sample > theMax) theMax = sample;
+	theSum += sample;
+      }
     }
-  }
+
+    if (rows == 0) {
+      if (calcMin) {
+	fprintf(stderr,"*info* min value %lf\n", theMin);
+      }
+      if (calcMean) {
+	fprintf(stderr,"*info* mean value %lf\n", theSum / samples);
+      }
+      if (calcMax) {
+	fprintf(stderr,"*info* max value %lf\n", theMax);
+      }
+    } else {
+      fprintf(stdout,"%zd\t%lf\t%lf\t%lf\n", r, theMin, theSum/samples, theMax);
+    }
+    
+  } while (r < rows);
 
   histFree(&h);
-  if (calcMin) {
-    fprintf(stderr,"*info* min value %lf\n", theMin);
-  }
-  if (calcMean) {
-    fprintf(stderr,"*info* mean value %lf\n", theSum / samples);
-  }
-  if (calcMax) {
-    fprintf(stderr,"*info* max value %lf\n", theMax);
-  }
   return 0;
 }
