9276e2b0c2c5313600609d6b152fca2e84a8920f
Stuart Inglis <stuart.inglis@gmail.com>
2023-09-12 21:44:09 +0000

    update

--- spit/scripts/blockSize-vs-IOPS.sh
+++ spit/scripts/blockSize-vs-IOPS.sh
@@ -8,22 +8,22 @@ do
     done > iops_mp_data.${a}
 done 
 
-#echo "set xrange auto" > blockSize-vs-IOPS.gnu
-#echo "set yrange auto" >> blockSize-vs-IOPS.gnu
-echo "set term dumb 100,40" > blockSize-vs-IOPS.gnu
-echo "set key outside" >> blockSize-vs-IOPS.gnu
-echo "set key tmargin center horiz" >> blockSize-vs-IOPS.gnu
-echo "set grid" >> blockSize-vs-IOPS.gnu
-echo "set xlabel 'Block Size (KiB)'" >> blockSize-vs-IOPS.gnu
-echo "set ylabel 'IOPS'" >> blockSize-vs-IOPS.gnu
-echo "set title 'spit: IOPS Test'" >> blockSize-vs-IOPS.gnu
-echo "set log x" >> blockSize-vs-IOPS.gnu
-echo "set xtics 2" >> blockSize-vs-IOPS.gnu
-echo "set log y" >> blockSize-vs-IOPS.gnu
-echo "set style line 1 pt 2" >> blockSize-vs-IOPS.gnu
-#echo "set term svg" >> blockSize-vs-IOPS.gnu
-#echo "set output 'iops-bs-2d.svg'" >> blockSize-vs-IOPS.gnu
-echo "plot 'iops_mp_data.r' with linespoints title '100/0', 'iops_mp_data.rwwwwwwwwwwwwwwwwwww' with linespoints title '5/95', 'iops_mp_data.rrrrrrrwwwwwwwwwwwww' with linespoints title '35/65', 'iops_mp_data.rw' with linespoints title '50/50', 'iops_mp_data.rrrrrrrrrrrrrwwwwwww' with linespoints title '65/35', 'iops_mp_data.rrrrrrrrrrrrrrrrrrrw' with linespoints title '95/5', 'iops_mp_data.w' with linespoints title '0/100' " >> blockSize-vs-IOPS.gnu
+#echo "set xrange auto" > out.gnuplot
+#echo "set yrange auto" >> out.gnuplot
+echo "set term dumb 100,40" > out.gnuplot
+echo "set key outside" >> out.gnuplot
+echo "set key tmargin center horiz" >> out.gnuplot
+echo "set grid" >> out.gnuplot
+echo "set xlabel 'Block Size (KiB)'" >> out.gnuplot
+echo "set ylabel 'IOPS'" >> out.gnuplot
+echo "set title 'spit: IOPS Test'" >> out.gnuplot
+echo "set log x" >> out.gnuplot
+echo "set xtics 2" >> out.gnuplot
+echo "set log y" >> out.gnuplot
+echo "set style line 1 pt 2" >> out.gnuplot
+#echo "set term svg" >> out.gnuplot
+#echo "set output 'iops-bs-2d.svg'" >> out.gnuplot
+echo "plot 'iops_mp_data.r' with linespoints title '100/0', 'iops_mp_data.rwwwwwwwwwwwwwwwwwww' with linespoints title '5/95', 'iops_mp_data.rrrrrrrwwwwwwwwwwwww' with linespoints title '35/65', 'iops_mp_data.rw' with linespoints title '50/50', 'iops_mp_data.rrrrrrrrrrrrrwwwwwww' with linespoints title '65/35', 'iops_mp_data.rrrrrrrrrrrrrrrrrrrw' with linespoints title '95/5', 'iops_mp_data.w' with linespoints title '0/100' " >> out.gnuplot
 
 
 
@@ -34,4 +34,4 @@ cat iops_mp_data.rrrrrrrrrrrrrwwwwwww | awk '{print "65",$1,$2}' >> d
 cat iops_mp_data.w | awk '{print "100",$1,$2}' >> d
 
 
-gnuplot 'blockSize-vs-IOPS.gnu'
+gnuplot 'out.gnuplot'
