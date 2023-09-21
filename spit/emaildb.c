85054e0adb5b8ecc0144a5df85308838d1ef71fc
Stuart Inglis <stuart.inglis@gmail.com>
2023-09-21 18:08:17 +1200

    new

---
+++ spit/emaildb.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+
+#include "emaildb.h"
+
+emaildbType* emaildbInit() {
+  emaildbType *d = calloc(sizeof(emaildbType), 1);
+  return d;
+}
+
+
+void emaildbAdd(emaildbType *e, char *emailaddress) {
+  for (size_t i = 0; i < e->len; i++) {
+    if (strcasecmp(emailaddress, e->addr[i])==0) {
+      // match
+      return;
+    }
+  }
+  e->len++;
+  e->addr = realloc(e->addr, (e->len) * sizeof(char*));
+  e->addr[e->len-1] = strdup(emailaddress);
+}
+
+void emaildbDump(emaildbType *e) {
+  for (size_t i = 0; i < e->len; i++) {
+    printf("%s\n", e->addr[i]);
+  }
+  fprintf(stderr, "[%zd]\n", e->len);
+}
+
+emaildbType * emaildbLoad(FILE *stream) {
+  emaildbType * e = emaildbInit();
+  
+  if (stream) {
+    char *line = NULL;
+    size_t linelen = 0;
+    ssize_t nread;
+    
+    while ((nread = getline(&line, &linelen, stream)) != -1) {
+
+      if (nread > 1) {
+	if (strchr(line, '@')) {
+	  // needs an @ at least!
+	  line[nread-1] = 0;
+	  emaildbAdd(e, line);
+	}
+      }
+    }
+    
+    free(line);
+  } else {
+    perror("problem");
+  }
+  
+  return e;
+}
+  
+
+void emaildbFree(emaildbType *e) {
+  for (size_t i = 0; i < e->len; i++) {
+    free(e->addr[i]);
+  }
+  free(e->addr);
+  memset(e, 0, sizeof(emaildbType));
+}
---
+++ spit/emaildb.h
@@ -0,0 +1,20 @@
+#ifndef __EMAILDB_H
+#define __EMAILDB_H
+
+
+typedef struct {
+  size_t len;
+  char **addr;
+} emaildbType;
+
+emaildbType * emaildbInit();
+
+void emaildbAdd(emaildbType *e, char *emailaddress);
+
+void emaildbDump(emaildbType *e);
+
+emaildbType * emaildbLoad(FILE *fp);
+
+void emaildbFree(emaildbType *e);
+
+#endif
