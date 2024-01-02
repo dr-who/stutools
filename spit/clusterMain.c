f2b991e4df6c90436be3569225ff6eb8487451ab
Stuart Inglis <stuart.inglis@gmail.com>
2024-01-02 00:08:59 +0000

    update

--- spit/clusterMain.c
+++ spit/clusterMain.c
@@ -6,14 +6,23 @@
 int keepRunning = 1;
 
 int main() {
+  char * s=clusterDumpJSON(NULL);
+  printf("%s", s);
+  free(s);
+
   clusterType *c = clusterInit(1600);
+
+  s=clusterDumpJSON(c);
+  printf("%s", s);
+  free(s);
+  
   clusterAddNode(c, "stu");
   clusterAddNode(c, "nic");
   clusterAddNode(c, "nic");
 
   clusterAddNodesIP(c, "stu", "172.17.0.2");
   clusterAddNodesIP(c, "stu", "172.17.3.2");
-  char * s=clusterDumpJSON(c);
+  s=clusterDumpJSON(c);
   printf("%s", s);
   free(s);
 
