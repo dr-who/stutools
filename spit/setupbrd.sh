60d046c194b30f152b10a6d6ec96fd91c46bfa31
Stuart Inglis <stuart.inglis@gmail.com>
2024-02-21 18:43:00 +1300

    update

--- spit/setupbrd.sh
+++ spit/setupbrd.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-modprobe brd nr_rd=32 rd_size=104857600
+modprobe brd nr_rd=128 rd_size=104857600
 
 /bin/rm -f /etc/exports
 
-for f in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
+for f in {0..127}
 do
 
     umount /mnt/disk$f
